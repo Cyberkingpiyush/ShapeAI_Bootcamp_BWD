7a797895c208bc7b849560138e423aed79f9ac99
Cyberkingpiyush <85407269+Cyberkingpiyush@users.noreply.github.com>
2021-06-05 21:54:46 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>This was An amazing taken by Shaurya Sir. We covered everything from Scratch including JavaScript React.js, HTML.</p>
+    </div>
+  );
+}
+
+export default Note;
